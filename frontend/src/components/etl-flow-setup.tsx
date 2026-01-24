8b5f59ed57e3c56237933a96cf9513e638e96b26
klimjs <klimsemenov86@gmail.com>
2026-01-24 23:47:36 +0100

    feat(frontend): mapping tab disabled condition

--- frontend/src/components/etl-flow-setup.tsx
+++ frontend/src/components/etl-flow-setup.tsx
@@ -2,8 +2,13 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs'
 import { Settings } from '@/components/settings'
 import { Mapping } from '@/components/mapping'
 import { Preview } from '@/components/preview'
+import { useWarehouseStore } from '@/store/warehouse-store'
 
 export const ETLFlowSetup = () => {
+  const { connectionStatus, selectedTable } = useWarehouseStore()
+
+  const isMappingEnabled = connectionStatus === 'success' && !!selectedTable
+
   return (
     <div className="flex flex-col gap-1 mx-auto max-w-2xl min-w-sm p-4">
       <Tabs defaultValue="settings">
@@ -11,7 +16,11 @@ export const ETLFlowSetup = () => {
           <TabsTrigger value="settings" className="px-4">
             Settings
           </TabsTrigger>
-          <TabsTrigger value="mapping" className="px-4">
+          <TabsTrigger
+            value="mapping"
+            className="px-4"
+            disabled={!isMappingEnabled}
+          >
             Mapping
           </TabsTrigger>
           <TabsTrigger value="preview" className="px-4">
