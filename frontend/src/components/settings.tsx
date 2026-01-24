b31d22641d65a16f2a3fa9b582ac9629bc2c42a5
klimjs <klimsemenov86@gmail.com>
2026-01-24 15:47:02 +0100

    feat(frontend): tables select ui

--- frontend/src/components/settings.tsx
+++ frontend/src/components/settings.tsx
@@ -5,28 +5,65 @@ import {
   InputGroupInput,
   InputGroupButton,
 } from '@/components/ui/input-group'
-import { Field, FieldLabel } from '@/components/ui/field'
+import { Field, FieldLabel, FieldGroup } from '@/components/ui/field'
 import { Database } from 'lucide-react'
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+} from '@/components/ui/select'
 
 export const Settings = () => {
+  // Test tables
+  const tables = [
+    { label: 'Select a table', value: null },
+    { label: 'Users', value: 'users' },
+    { label: 'Customers', value: 'customers' },
+    { label: 'Orders', value: 'orders' },
+  ]
+
   return (
     <Card className="py-6">
       <CardContent>
-        <Field>
-          <FieldLabel htmlFor="connection">Database connection</FieldLabel>
-          <InputGroup>
-            <InputGroupInput
-              id="connection"
-              placeholder="postgresql://postgres:postgres@server:9999/database"
-            />
-            <InputGroupAddon>
-              <Database />
-            </InputGroupAddon>
-            <InputGroupAddon align="inline-end">
-              <InputGroupButton>Test</InputGroupButton>
-            </InputGroupAddon>
-          </InputGroup>
-        </Field>
+        <FieldGroup>
+          <Field>
+            <FieldLabel htmlFor="connection">Database connection</FieldLabel>
+            <InputGroup>
+              <InputGroupInput
+                id="connection"
+                placeholder="postgresql://postgres:postgres@server:9999/database"
+              />
+              <InputGroupAddon>
+                <Database />
+              </InputGroupAddon>
+              <InputGroupAddon align="inline-end">
+                <InputGroupButton>Test</InputGroupButton>
+              </InputGroupAddon>
+            </InputGroup>
+          </Field>
+
+          <Field>
+            <FieldLabel>Table</FieldLabel>
+
+            <Select items={tables} disabled={false}>
+              <SelectTrigger className="max-w-64">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  {tables.map((table) => (
+                    <SelectItem key={table.value} value={table.value}>
+                      {table.label}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </Field>
+        </FieldGroup>
       </CardContent>
     </Card>
   )
